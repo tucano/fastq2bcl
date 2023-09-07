d746408adfe2dfdc20e6bb771524eba125e79150
Davide Rambaldi <davide.rambaldi@fht.org>
2023-09-07 11:43:45 +0200

    tests place holders

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,9 +10,9 @@ def test_main_usage(capsys):
     """CLI Tests"""
     # capsys is a pytest fixture that allows asserts against stdout/stderr
     # https://docs.pytest.org/en/stable/capture.html
-    main([])
+    main(["data/flowcell_example/Sample1_S1_L001_R1_001.fastq.gz"])
     captured = capsys.readouterr()
-    assert "usage" in captured.out
+    assert "RUNID: YYMMDD_M11111_0222_000000000-K9H97" in captured.out
 
 def test_mock_run_id():
     """Mock run id Tests"""
