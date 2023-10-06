657be44f74a59da63ae41ea5e904f8695617b75f
Davide Rambaldi <davide.rambaldi@fht.org>
2023-10-06 15:00:27 +0200

    added bcl2fastq to build_flowcells script

--- scripts/build_flowcells.sh
+++ scripts/build_flowcells.sh
@@ -4,26 +4,39 @@
 
 BUILD_DIR='build_flowcells'
 TEST_DIR='data/test'
+BCL2FASTQ=`realpath scripts/bcl2fastq_docker.sh`
 
 echo "Creating flowcells in BUILD_DIR $BUILD_DIR"
 
-mkdir -p $BUILD_DIR
+# mkdir -p $BUILD_DIR
 
-for dir in $TEST_DIR/*
+# for dir in $TEST_DIR/*
+# do
+#     dirname=`basename $dir`
+#     #echo "Dir $dir basename $dirname creating corresponding dir in $BUILD_DIR"
+#     mkdir -p $BUILD_DIR/$dirname
+#     INPUT_FILES="$dir"/*.gz
+#     # check if there is a mask
+#     mask="$dir"/mask.txt
+
+#     if [ -f "$mask" ]; then
+#         mask_string=`cat $mask`
+#         mask_arg="-m $mask_string"
+#         echo "FOUNDED MASK $mask_arg"
+#     fi
+#     fastq2bcl -o $BUILD_DIR/$dirname $mask_arg $INPUT_FILES
+#     RUNDIR="$BUILD_DIR/$dirname"/YYMMDD_*
+#     cp $dir/*.csv $RUNDIR
+# done
+
+PWD_DIR=`pwd -P`
+
+for dir in $BUILD_DIR/*/*
 do
-    dirname=`basename $dir`
-    #echo "Dir $dir basename $dirname creating corresponding dir in $BUILD_DIR"
-    mkdir -p $BUILD_DIR/$dirname
-    INPUT_FILES="$dir"/*.gz
-    # check if there is a mask
-    mask="$dir"/mask.txt
+    cd $dir
+    echo "IN DIRECTORY $dir"
 
-    if [ -f "$mask" ]; then
-        mask_string=`cat $mask`
-        mask_arg="-m $mask_string"
-        echo "FOUNDED MASK $mask_arg"
-    fi
-    fastq2bcl -o $BUILD_DIR/$dirname $mask_arg $INPUT_FILES
-    RUNDIR="$BUILD_DIR/$dirname"/YYMMDD_*
-    cp $dir/*.csv $RUNDIR
+    $BCL2FASTQ
+
+    cd $PWD_DIR
 done
