85cce8551b55275647cc86713199bcc5baeb30c9
Davide Rambaldi <davide.rambaldi@fht.org>
2023-10-09 11:34:56 +0200

    build flowcells and verify

--- scripts/build_flowcells.sh
+++ scripts/build_flowcells.sh
@@ -6,37 +6,62 @@ BUILD_DIR='build_flowcells'
 TEST_DIR='data/test'
 BCL2FASTQ=`realpath scripts/bcl2fastq_docker.sh`
 
-echo "Creating flowcells in BUILD_DIR $BUILD_DIR"
+echo "Creating flowcells in directory $BUILD_DIR"
 
+PWD_DIR=`pwd -P`
 mkdir -p $BUILD_DIR
 
 for dir in $TEST_DIR/*
 do
     dirname=`basename $dir`
-    #echo "Dir $dir basename $dirname creating corresponding dir in $BUILD_DIR"
-    mkdir -p $BUILD_DIR/$dirname
-    INPUT_FILES="$dir"/*.gz
-    # check if there is a mask
-    mask="$dir"/mask.txt
+    echo "flowcell $dirname"
 
-    if [ -f "$mask" ]; then
-        mask_string=`cat $mask`
-        mask_arg="-m $mask_string"
-        echo "FOUNDED MASK $mask_arg"
+    # source test.config
+    if [ -f "$dir/test.config" ]; then
+        mkdir -p $BUILD_DIR/$dirname
+        source $dir/test.config
+        echo "Expected file $expected_file"
+        INPUT_FILES="$dir"/*.gz
+        echo "Input files: $INPUT_FILES"
+
+        # check if there is a mask defined in test.config
+        if [ -z ${MASK} ]; then
+            echo "The MASK is unset."
+        else
+            mask_arg="-m $MASK"
+            echo "Mask: $MASK mask_arg: $mask_arg"
+        fi
+
+        # build flowcell
+        fastq2bcl -o $BUILD_DIR/$dirname $mask_arg $INPUT_FILES > $BUILD_DIR/$dirname/fastq2blc.log 2>&1
+
+        # resulting RUNDIR
+        RUNDIR="$BUILD_DIR/$dirname"/YYMMDD_*
+        echo "copying SampleSheet to $RUNDIR"
+        cp $dir/*.csv $RUNDIR
+
+        # test bcl2fastq
+        echo "Running bcl2fastq in flowcell $RUNDIR"
+        cd $RUNDIR
+        echo "running bcl2fastq in dir $PWD"
+        $BCL2FASTQ > bcl2fastq.log 2>&1
+        echo "$dirname [OK]"
+        cd $PWD_DIR
+
+        # check output
+        zdiff $expected_file $BUILD_DIR/$output_file > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo "Output $output_file [OK]"
+        else
+            echo "Output $output_file differ."
+        fi
+
+        unset MASK
+        unset expected_file
+        unset output_file
+    else
+        echo "Missing test.config. Skipping ..."
     fi
-    fastq2bcl -o $BUILD_DIR/$dirname $mask_arg $INPUT_FILES
-    RUNDIR="$BUILD_DIR/$dirname"/YYMMDD_*
-    cp $dir/*.csv $RUNDIR
-done
 
-PWD_DIR=`pwd -P`
-
-for dir in $BUILD_DIR/*/*
-do
-    cd $dir
-    echo "IN DIRECTORY $dir"
-
-    $BCL2FASTQ
-
-    cd $PWD_DIR
+    echo "----------------------"
 done
