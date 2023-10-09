e7bd7166d22d5e4c97c6ebb03baecf32a6109290
Davide Rambaldi <davide.rambaldi@fht.org>
2023-10-09 16:09:57 +0200

    build flowcells and test with bcl2fastq using a build.config file

--- scripts/build_flowcells.sh
+++ scripts/build_flowcells.sh
@@ -16,51 +16,47 @@ do
     dirname=`basename $dir`
     echo "flowcell $dirname"
 
-    # source test.config
-    if [ -f "$dir/test.config" ]; then
+    # source build.config
+    if [ -f "$dir/build.config" ]; then
         mkdir -p $BUILD_DIR/$dirname
-        source $dir/test.config
+        source $dir/build.config
         echo "Expected file $expected_file"
-        INPUT_FILES="$dir"/*.gz
-        echo "Input files: $INPUT_FILES"
 
-        # check if there is a mask defined in test.config
-        if [ -z ${MASK} ]; then
-            echo "The MASK is unset."
+        # check if there are command_args defined in build.config
+        if [[ -z ${command_args} ]]; then
+            echo "Error: command_args is unset."
+            exit 1
         else
-            mask_arg="-m $MASK"
-            echo "Mask: $MASK mask_arg: $mask_arg"
+            # build flowcell
+            fastq2bcl -o $BUILD_DIR/$dirname $command_args > $BUILD_DIR/$dirname/fastq2blc.log 2>&1
+
+            # resulting RUNDIR
+            RUNDIR="$BUILD_DIR/$dirname"/YYMMDD_*
+            echo "copying SampleSheet to $RUNDIR"
+            cp $dir/*.csv $RUNDIR
+
+            # test bcl2fastq
+            echo "Running bcl2fastq in flowcell $RUNDIR"
+            cd $RUNDIR
+            echo "running bcl2fastq in dir $PWD"
+            $BCL2FASTQ > bcl2fastq.log 2>&1
+            echo "$dirname [OK]"
+            cd $PWD_DIR
+
+            # check output
+            zdiff $expected_file $BUILD_DIR/$output_file > /dev/null 2>&1
+            if [ $? -eq 0 ]; then
+                echo "Output $output_file [OK]"
+            else
+                echo "Output $output_file differ."
+            fi
         fi
 
-        # build flowcell
-        fastq2bcl -o $BUILD_DIR/$dirname $mask_arg $INPUT_FILES > $BUILD_DIR/$dirname/fastq2blc.log 2>&1
-
-        # resulting RUNDIR
-        RUNDIR="$BUILD_DIR/$dirname"/YYMMDD_*
-        echo "copying SampleSheet to $RUNDIR"
-        cp $dir/*.csv $RUNDIR
-
-        # test bcl2fastq
-        echo "Running bcl2fastq in flowcell $RUNDIR"
-        cd $RUNDIR
-        echo "running bcl2fastq in dir $PWD"
-        $BCL2FASTQ > bcl2fastq.log 2>&1
-        echo "$dirname [OK]"
-        cd $PWD_DIR
-
-        # check output
-        zdiff $expected_file $BUILD_DIR/$output_file > /dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo "Output $output_file [OK]"
-        else
-            echo "Output $output_file differ."
-        fi
-
-        unset MASK
         unset expected_file
         unset output_file
+        unset command_args
     else
-        echo "Missing test.config. Skipping ..."
+        echo "Missing build.config. Skipping ..."
     fi
 
     echo "----------------------"
